69df1a3680292b2030fc47cc7e615dc362eda8c2
= <contato@carloshb.com.br>
2018-02-18 16:09:39 -0300

    Refactored Box component

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,19 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class Box extends Component
-{
-    render()
-    {
-        return <section className="box">
-            <header>
-                <h1>{ this.props.title }</h1>
-            </header>
-            { this.props.children }
-            <footer>
-                { this.props.footer }
-            </footer>
-        </section>;
-    }
-}
+import Label from './Label';
+
+const Box = ({ planet, population, climate, terrain, featured }) => <section className="box">
+    <header>
+        <h1>{ planet }</h1>
+    </header>
+    <div className="content">
+        <Label title='POPULATION'>{ population }</Label>
+        <Label title='CLIMATE'>{ climate }</Label>
+        <Label title='TERRAIN'>{ terrain }</Label>
+    </div>
+    <footer>
+        Featured in { featured } films.
+    </footer>
+</section>;
+
+Box.propTypes = {
+    planet: PropTypes.string.isRequired,
+    population: PropTypes.number.isRequired,
+    climate: PropTypes.string.isRequired,
+    terrain: PropTypes.string.isRequired,
+    featured: PropTypes.number.isRequired,
+};
 
 export default Box;
