543fe3e14bfdcadd549100a0c397f500e3ad92ec
= <contato@carloshb.com.br>
2018-02-19 09:56:58 -0300

    Add property color in Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button  = ({ text, action }) => <button onClick={action}>
+const Button  = ({ text, action, color }) => <button className={color} onClick={action}>
     { text }
 </button>;
 
 Button.propTypes = {
     text: PropTypes.string.isRequired,
-    action: PropTypes.func.isRequired
+    action: PropTypes.func.isRequired,
+    color: PropTypes.string
+};
+
+Button.defaultProps = {
+    color: 'red'
 };
 
 export default Button;
