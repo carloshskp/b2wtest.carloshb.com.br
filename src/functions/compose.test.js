579a70e0c9069463bdec81fc54a43c4fcc3dc594
= <contato@carloshb.com.br>
2018-02-19 06:43:00 -0300

    Add compose function test

---
+++ src/functions/compose.test.js
@@ -0,0 +1,12 @@
+import compose from './compose';
+
+it('compose functions', () => {
+
+    const subtractOne = num => num - 1;
+    const sumTwo = num => num + 2;
+    const sumTwoAndSubtractOne = compose(subtractOne, sumTwo);
+    const expected = 6;
+    const tree = sumTwoAndSubtractOne(5);
+
+    expect(tree).toEqual(expected);
+});
